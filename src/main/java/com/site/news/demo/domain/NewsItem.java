7f54e9b138d2b2d3dda103ae71e2905a38308c85
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-16 14:29:25 +0300

    added new dependency for mysql, change application properties file and created new news item entity

---
+++ src/main/java/com/site/news/demo/domain/NewsItem.java
@@ -0,0 +1,79 @@
+package com.site.news.demo.domain;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import java.sql.Date;
+
+@Entity
+public class NewsItem {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    private String title;
+    private String tag;
+    private String image;
+    private String content;
+    Date date;
+
+    public NewsItem() {}
+
+    public NewsItem(Integer id, String title, String tag, String image, String content, Date date) {
+        this.id = id;
+        this.title = title;
+        this.tag = tag;
+        this.image = image;
+        this.content = content;
+        this.date = date;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getTag() {
+        return tag;
+    }
+
+    public void setTag(String tag) {
+        this.tag = tag;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+}
