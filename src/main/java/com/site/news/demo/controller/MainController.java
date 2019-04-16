f911647803bd72fed13e728739f764af9be9e306
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-16 15:18:52 +0300

    added simple mapping for news items

--- src/main/java/com/site/news/demo/controller/MainController.java
+++ src/main/java/com/site/news/demo/controller/MainController.java
@@ -1,14 +1,25 @@
 package com.site.news.demo.controller;
 
+import com.site.news.demo.domain.NewsItem;
+import com.site.news.demo.repository.NewsItemRepo;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import sun.plugin2.message.Message;
+
+import java.util.Map;
 
 @Controller
 public class MainController {
+    @Autowired
+    private NewsItemRepo newsItemRepo;
 
     @GetMapping("/")
-    public String main(Model model){
+    public String main(Map<String,Object> model){
+        Iterable<NewsItem>newsItems=newsItemRepo.findAll();
+        model.put("newsItem",newsItems);
         return "main";
     }
+    
 }
