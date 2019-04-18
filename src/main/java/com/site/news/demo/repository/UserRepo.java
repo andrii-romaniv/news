75e5b038a1c888fdcb3b32fa8cf1c91be4694c9d
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-18 19:18:06 +0300

    added postmaping to registration controller

--- src/main/java/com/site/news/demo/repository/UserRepo.java
+++ src/main/java/com/site/news/demo/repository/UserRepo.java
@@ -1,4 +1,8 @@
 package com.site.news.demo.repository;
 
-public interface UserRepo {
+import com.site.news.demo.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepo extends JpaRepository<User,Long> {
+   // User findByUsername(String username);
 }
