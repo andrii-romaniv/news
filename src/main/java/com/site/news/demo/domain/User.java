bba1e7478ec2cd3ce68099cc650a4b4380b1a6d6
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-18 18:32:53 +0300

    added getters, setters and constructor to user class

--- src/main/java/com/site/news/demo/domain/User.java
+++ src/main/java/com/site/news/demo/domain/User.java
@@ -31,4 +31,60 @@ public class User {
 
     @Lob
     private byte[] image;
+
+    public User() {}
+
+    public User(String userName, String email, String password) {
+        this.userName = userName;
+        this.email = email;
+        this.password = password;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Authority getAuthority() {
+        return authority;
+    }
+
+    public void setAuthority(Authority authority) {
+        this.authority = authority;
+    }
+
+    public byte[] getImage() {
+        return image;
+    }
+
+    public void setImage(byte[] image) {
+        this.image = image;
+    }
 }
