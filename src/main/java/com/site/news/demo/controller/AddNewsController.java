ec0e02209bae1e22b40a917dec6afa3b0623ccf2
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-19 12:34:06 +0300

    added simple changes to add news controller

--- src/main/java/com/site/news/demo/controller/AddNewsController.java
+++ src/main/java/com/site/news/demo/controller/AddNewsController.java
@@ -4,15 +4,12 @@ import com.site.news.demo.domain.NewsItem;
 import com.site.news.demo.repository.NewsItemRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.multipart.MultipartFile;
 
-import javax.validation.Valid;
 import java.io.File;
 import java.io.IOException;
 import java.util.UUID;
@@ -32,11 +29,8 @@ public class AddNewsController {
     }
 
     @PostMapping("/addnews")
-    public String addNewNews(NewsItem newsItem,
-                                 @RequestParam("image_file") MultipartFile image,
-                                 Model model
+    public String addNewNews(NewsItem newsItem,@RequestParam("image_file") MultipartFile image
     ) throws IOException {
-        ;
         if(image!=null && !image.getOriginalFilename().isEmpty()){
             File uploadDirectory=new File(uploadPath);
             if(!uploadDirectory.exists())uploadDirectory.mkdir();
@@ -46,7 +40,6 @@ public class AddNewsController {
             newsItem.setImage(resultFilename);
         }
         newsItemRepo.save(newsItem);
-        model.addAttribute("newsItem",newsItemRepo.findAll());
-        return "main";
+        return "redirect:/";
     }
 }
