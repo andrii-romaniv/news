4e96ff7752667548f3dc22c2e0cd9345e895404a
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-19 18:28:29 +0300

    added corrections

--- src/main/java/com/site/news/demo/config/WebSecurityConfig.java
+++ src/main/java/com/site/news/demo/config/WebSecurityConfig.java
@@ -15,7 +15,6 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
         http
                 .authorizeRequests()
-//                .antMatchers("/","/registration").permitAll()
                 .antMatchers("/","/registration").permitAll()
                 .anyRequest().authenticated()
                 .and()
--- src/main/java/com/site/news/demo/repository/UserRepo.java
+++ src/main/java/com/site/news/demo/repository/UserRepo.java
@@ -4,5 +4,4 @@ import com.site.news.demo.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface UserRepo extends JpaRepository<User,Long> {
-   // User findByUsername(String username);
 }
