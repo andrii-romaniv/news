f1e04509c42260dfedca7e1faa669fc3436abb24
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-20 14:49:41 +0300

    added annotation @service in userservice and added authentication from db

--- src/main/java/com/site/news/demo/config/WebSecurityConfig.java
+++ src/main/java/com/site/news/demo/config/WebSecurityConfig.java
@@ -1,5 +1,6 @@
 package com.site.news.demo.config;
 
+import com.site.news.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,10 +8,13 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+    @Autowired
+    private UserService userService;
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
@@ -28,9 +32,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("user").password("pass").roles("USER")
-                .and()
-                .withUser("admin").password("pass").roles("ADMIN");
+//        auth.inMemoryAuthentication()
+//                .withUser("user").password("pass").roles("USER")
+//                .and()
+//                .withUser("admin").password("pass").roles("ADMIN");
+        auth.userDetailsService(userService).passwordEncoder(NoOpPasswordEncoder.getInstance());
     }
 }
