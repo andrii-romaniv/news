2a918e4cdeaf8055f5f185bd040b1e4e04bc9a50
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-22 21:00:06 +0300

    added Comment Entity

---
+++ src/main/java/com/site/news/demo/domain/Comment.java
@@ -0,0 +1,74 @@
+package com.site.news.demo.domain;
+
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@Data
+@EqualsAndHashCode
+@NoArgsConstructor
+@Entity
+@Table(name = "comments")
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @ManyToOne
+    @Column(name="user_id", nullable = false)
+    private User author;
+
+    @ManyToOne
+    @Column(name="news_item_id", nullable = false)
+    private NewsItem news;
+
+    @Column(name="content", nullable = false)
+    private String content;
+
+    @Column(name="create_date",nullable = false)
+    private Date data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public User getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(User author) {
+        this.author = author;
+    }
+
+    public NewsItem getNews() {
+        return news;
+    }
+
+    public void setNews(NewsItem news) {
+        this.news = news;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+}
