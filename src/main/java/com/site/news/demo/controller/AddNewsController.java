f2d78c475a5b229b0033b767b145f179897f0602
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-22 13:41:38 +0300

    added file service, refactoring

--- src/main/java/com/site/news/demo/controller/AddNewsController.java
+++ src/main/java/com/site/news/demo/controller/AddNewsController.java
@@ -1,7 +1,9 @@
 package com.site.news.demo.controller;
 
+import com.site.news.demo.config.EncodingConfig;
 import com.site.news.demo.domain.NewsItem;
 import com.site.news.demo.repository.NewsItemRepo;
+import com.site.news.demo.service.FileService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -31,18 +33,14 @@ public class AddNewsController {
         return "addnews";
     }
 
+    @Autowired
+    private FileService fileService;
+
     @PostMapping("/addnews")
     public String addNewNews(NewsItem newsItem,@RequestParam("file") MultipartFile image
     ) throws IOException {
-        if(image!=null && !image.getOriginalFilename().isEmpty()){
-            File uploadDirectory=new File(uploadPath);
-            if(!uploadDirectory.exists())uploadDirectory.mkdir();
-            String genereteName= UUID.randomUUID().toString();
-            String resultFilename=genereteName+"-"+image.getOriginalFilename();
-            image.transferTo(new File(uploadPath+"/"+resultFilename));
-            newsItem.setImage(resultFilename);
-            newsItem.setDate(new Date());
-        }
+        newsItem.setImage(fileService.uploadFile(image));
+        newsItem.setDate(new Date());
         newsItemRepo.save(newsItem);
         return "redirect:/";
     }
