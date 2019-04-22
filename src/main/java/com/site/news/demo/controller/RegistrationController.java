07697531820f2c016c7855ddf8596dcb0fa0c688
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-22 14:45:14 +0300

    added input image to user registration

--- src/main/java/com/site/news/demo/controller/RegistrationController.java
+++ src/main/java/com/site/news/demo/controller/RegistrationController.java
@@ -1,24 +1,21 @@
 package com.site.news.demo.controller;
 
-import com.site.news.demo.domain.Authority;
 import com.site.news.demo.domain.User;
-import com.site.news.demo.repository.UserRepo;
+import com.site.news.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
-import java.util.Collections;
+import java.io.IOException;
 
 @Controller
 public class RegistrationController {
-    @Autowired
-    private UserRepo userRepo;
 
     @Autowired
-    PasswordEncoder passwordEncoder;
+    private UserService userService;
 
     @GetMapping("/registration")
     public String registration(){
@@ -26,17 +23,9 @@ public class RegistrationController {
     }
 
     @PostMapping("/registration")
-    public String addNewUser(User user, Model model){
-        User userFromDB=userRepo.findByUsername(user.getUsername());
-        if(userFromDB!=null){
-            return "registration";
-        }
-        else{
-            if(userRepo.count()==0)user.setAuthority(Authority.ROLE_ADMIN);
-            else user.setAuthority(Authority.ROLE_USER);
-            user.setPassword(passwordEncoder.encode(user.getPassword()));
-            userRepo.save(user);
-        }
-        return "redirect:/login";
+    public String addNewUser(User user, @RequestParam("file") MultipartFile image) throws IOException {
+        if(userService.addUser(user,image))return "redirect:/login";
+        else return "registration";
     }
+
 }
--- src/main/java/com/site/news/demo/service/UserService.java
+++ src/main/java/com/site/news/demo/service/UserService.java
@@ -1,19 +1,46 @@
 package com.site.news.demo.service;
 
+import com.site.news.demo.domain.Authority;
+import com.site.news.demo.domain.User;
 import com.site.news.demo.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
+
+import java.io.IOException;
 
 @Service
 public class UserService implements UserDetailsService {
     @Autowired
     private UserRepo userRepo;
 
+    @Autowired
+    PasswordEncoder passwordEncoder;
+
+    @Autowired
+    FileService fileService;
+
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         return userRepo.findByUsername(username);
+    }
+
+    public boolean addUser(User user,MultipartFile image) throws IOException {
+        User userFromDB=userRepo.findByUsername(user.getUsername());
+        if(userFromDB!=null){
+            return false;
+        }
+        else{
+            if(userRepo.count()==0)user.setAuthority(Authority.ROLE_ADMIN);
+            else user.setAuthority(Authority.ROLE_USER);
+            user.setPassword(passwordEncoder.encode(user.getPassword()));
+            user.setImage(fileService.uploadFile(image));
+            userRepo.save(user);
+            return true;
+        }
     }
 }
