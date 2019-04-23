d10d7c21f71160ec84c087fe0ef23361d463b662
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-23 23:01:24 +0300

    added new method findAllByNewsOrderByIdDesc to CommentRepo

--- src/main/java/com/site/news/demo/repository/CommentRepo.java
+++ src/main/java/com/site/news/demo/repository/CommentRepo.java
@@ -1,7 +1,11 @@
 package com.site.news.demo.repository;
 
 import com.site.news.demo.domain.Comment;
+import com.site.news.demo.domain.NewsItem;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface CommentRepo extends JpaRepository<Comment,Long> {
+    List<Comment> findAllByNewsOrderByIdDesc(NewsItem newsItem);
 }
