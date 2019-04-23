bfa2811bc02a334f36a87fab866c9a69d48b8966
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-23 22:56:56 +0300

    changed method addUser and added new method in userService. Changed content for main page

--- src/main/java/com/site/news/demo/service/UserService.java
+++ src/main/java/com/site/news/demo/service/UserService.java
@@ -4,6 +4,7 @@ import com.site.news.demo.domain.Authority;
 import com.site.news.demo.domain.User;
 import com.site.news.demo.repository.UserRepo;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
@@ -29,18 +30,24 @@ public class UserService implements UserDetailsService {
         return userRepo.findByUsername(username);
     }
 
-    public boolean addUser(User user,MultipartFile image) throws IOException {
-        User userFromDB=userRepo.findByUsername(user.getUsername());
-        if(userFromDB!=null){
-            return false;
-        }
-        else{
+    public User addUser(User user,MultipartFile image) throws IOException {
+        if(user==null )return null;
+        else
+            if(userRepo.findByUsername(user.getUsername())!=null)return null;
+                else{
             if(userRepo.count()==0)user.setAuthority(Authority.ROLE_ADMIN);
             else user.setAuthority(Authority.ROLE_USER);
             user.setPassword(passwordEncoder.encode(user.getPassword()));
             user.setImage(fileService.uploadFile(image));
             userRepo.save(user);
-            return true;
+            return user;
         }
+    }
+
+    public User getCorrectUser(){
+        return  (User) SecurityContextHolder
+                .getContext()
+                .getAuthentication()
+                .getPrincipal();
     }
 }
