7399bf5fa4eb1cef7059368a1fcc8a89727f97a0
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-04-23 23:05:02 +0300

    added comments on news page

--- src/main/java/com/site/news/demo/controller/NewsPageController.java
+++ src/main/java/com/site/news/demo/controller/NewsPageController.java
@@ -2,15 +2,15 @@ package com.site.news.demo.controller;
 
 import com.site.news.demo.domain.Comment;
 import com.site.news.demo.domain.NewsItem;
-import com.site.news.demo.domain.User;
 import com.site.news.demo.repository.CommentRepo;
 import com.site.news.demo.repository.NewsItemRepo;
+import com.site.news.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
 
+import javax.servlet.http.HttpServletRequest;
 import java.util.Date;
 
 @Controller
@@ -23,18 +23,28 @@ public class NewsPageController {
     @Autowired
     CommentRepo commentRepo;
 
+    @Autowired
+    UserService userService;
+
     @GetMapping("{title}")
     public String newsPage(@PathVariable String title, Model model){
+        NewsItem newsItem=newsItemRepo.findByTitle(title);
         model.addAttribute("newsItem",newsItemRepo.findByTitle(title));
+        model.addAttribute("comments",commentRepo.findAllByNewsOrderByIdDesc(newsItem));
         return "newsPage";
     }
 
     @PostMapping
-    public String addComment(@AuthenticationPrincipal User user, Comment comment,@RequestParam Long id){
-        comment.setAuthor(user);
-        comment.setNews(newsItemRepo.findOne(id));
-        comment.setData(new Date());
-        commentRepo.save(comment);
-        return "redirect:/";
+    public String addComment(Comment comment, @RequestParam Long id, HttpServletRequest request){
+        if(comment.getContent().isEmpty()) return "redirect:"+request.getHeader("referer");
+        else {
+            long commentId = commentRepo.count();
+            comment.setId(++commentId);
+            comment.setAuthor(userService.getCorrectUser());
+            comment.setNews(newsItemRepo.findOne(id));
+            comment.setData(new Date());
+            commentRepo.save(comment);
+            return "redirect:" + request.getHeader("referer");
+        }
     }
 }
