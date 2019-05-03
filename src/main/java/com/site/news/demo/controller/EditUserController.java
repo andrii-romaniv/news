fac384cf58252ceae88dde984b7c45bacb1282cc
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-05-03 22:11:30 +0300

    throw UserNotFoundException when user not found on EditUserController

--- src/main/java/com/site/news/demo/controller/EditUserController.java
+++ src/main/java/com/site/news/demo/controller/EditUserController.java
@@ -2,12 +2,15 @@ package com.site.news.demo.controller;
 
 import com.site.news.demo.domain.Authority;
 import com.site.news.demo.domain.User;
+import com.site.news.demo.exception.UserNotFoundException;
 import com.site.news.demo.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Optional;
 
 @Controller
 @RequestMapping("/edituser")
@@ -18,6 +21,8 @@ UserRepository userRepository;
 
     @GetMapping("{id}")
     public String editUser(@PathVariable long id,Model model){
+        Optional<User> user= Optional.ofNullable(userRepository.findOne(id));
+        if(!user.isPresent())throw new UserNotFoundException(id);
         model.addAttribute("user", userRepository.findOne(id));
         model.addAttribute("roles", Authority.values());
         return "editUser";
