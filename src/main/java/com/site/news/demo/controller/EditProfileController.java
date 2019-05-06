6cb6ef5d51749a5e5916a5d7a8d19d7962855c5d
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-05-06 17:58:05 +0300

    add validation on editProfile page

--- src/main/java/com/site/news/demo/controller/EditProfileController.java
+++ src/main/java/com/site/news/demo/controller/EditProfileController.java
@@ -1,13 +1,12 @@
 package com.site.news.demo.controller;
 
 import com.site.news.demo.domain.User;
-import com.site.news.demo.repository.UserRepository;
-import com.site.news.demo.service.FileServiceImplement;
 import com.site.news.demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -15,18 +14,11 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 import javax.validation.Valid;
 import java.io.IOException;
-import java.util.Optional;
 
 @Controller
 @RequestMapping("/settings")
 @PreAuthorize("#id == principal.id")
 public class EditProfileController {
-
-    @Autowired
-    UserRepository userRepository;
-
-    @Autowired
-    FileServiceImplement fileService;
 
     @Autowired
     UserService userService;
@@ -42,19 +34,21 @@ public class EditProfileController {
     @PostMapping("{id}")
     public String loadImage(@PathVariable long id,@RequestParam("file") MultipartFile image,
                             HttpServletRequest request) throws IOException {
-        Optional<User> user=userService.getUserObjectById(id);
-        user.get().setImage(fileService.uploadFile(image));
-        userRepository.save(user.get());
+        userService.updateProfileImage(id, image);
         return "redirect:" + request.getHeader("referer");
     }
 
     @PostMapping("/update")
-    public String updateProfile(@RequestParam long id,@Valid User user,HttpServletRequest request){
-        userService.updateUser(id,user);
-        HttpSession httpSession=request.getSession();
-        httpSession.invalidate();
-        return "redirect:/login";
+    public String updateProfile(@RequestParam long id, @Valid User user,
+                                BindingResult bindingResult,
+                                HttpServletRequest request){
+        if(bindingResult.hasErrors())return "editProfilePage";
+        else {
+            userService.updateUser(id, user);
+            HttpSession httpSession = request.getSession();
+            httpSession.invalidate();
+            return "redirect:/login";
+        }
     }
-
 
 }
