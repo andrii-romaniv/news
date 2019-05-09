117516602761e9258e339ca0f326c1a59334f769
Vasyl Gladkyy <vasil.gladkiy@gmail.com>
2019-05-09 15:55:18 +0300

    add integration tests for authorization in application

--- src/test/java/com/site/news/demo/AuthorizationTest.java
+++ src/test/java/com/site/news/demo/AuthorizationTest.java
@@ -1,4 +1,54 @@
 package com.site.news.demo;
 
-public class AuthorizationClass {
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestBuilders.formLogin;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.redirectedUrl;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+@AutoConfigureMockMvc
+@TestPropertySource("/application-test.properties")
+@Sql(value = {"/users-before.sql"}, executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD)
+@Sql(value = {"/users-after.sql"}, executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+public class AuthorizationTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    public void accessDeniedTest()throws Exception{
+        this.mockMvc.perform(get("/addnews"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("http://localhost/login"));
+    }
+
+    @Test
+    public void correctAuthorizationTest()throws Exception{
+        this.mockMvc.perform(formLogin().user("User").password("pass"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/"));
+    }
+
+    @Test
+    public void incorectAuthorizationTest()throws Exception
+    {
+        this.mockMvc.perform(formLogin().user("dontExistUser").password("null"))
+                .andDo(print())
+                .andExpect(status().is3xxRedirection())
+                .andExpect(redirectedUrl("/login?error"));
+    }
 }
